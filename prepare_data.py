7c2e2998b4bffb89cda6afe2c9df882798e7487d
l.fricke <l.fricke@frox-it.de>
2019-06-16 18:52:12 +0200

    is it learning???

--- prepare_data.py
+++ prepare_data.py
@@ -4,8 +4,8 @@ import cv2
 
 
 def canny(image, sigma=0.0):
-  gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-  grayed = np.where(gray < 20, 255, 0)
+  #gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+  grayed = np.where(image < 20, 255, 0)
 
   lower = sigma*128 - 128
   upper = 255
@@ -20,10 +20,9 @@ IN_DIR = 'faces'
 OUT_DIR = 'parsed_data'
 
 def parse_data():
-  if not os.path.exists(OUT_DIR):
-    os.makedirs(OUT_DIR)
-
   idx = 0
+  X = []
+  Y = []
   for root, subdirs, files in os.walk('faces'):
     for file in files:
       path = root + '/' + file
@@ -34,18 +33,26 @@ def parse_data():
       if image is None:
         assert(False)
 
+      print("parsing face {}".format(idx))
+
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
       image = cv2.resize(image, (IMAGE_W, IMAGE_H), interpolation= cv2.INTER_LINEAR)
 
-      out_path = OUT_DIR + '/' + "img_{}x{}_{}.png".format(IMAGE_W, IMAGE_H, idx)
-      
-      print("Parsing image {} to output file {}".format(path, out_path))
-      cv2.imwrite(out_path, image)
-
       sketch_img = canny(image, 0.1)
-      out_path = OUT_DIR + '/' + "img_{}x{}_{}_sketch.png".format(IMAGE_W, IMAGE_H, idx)
-      cv2.imwrite(out_path, sketch_img)
       
+      X.append(sketch_img)
+      Y.append(image)
+
       idx+=1
+  X = np.array(X).reshape(-1, IMAGE_W, IMAGE_H, 1)
+  Y = np.array(Y).reshape(-1, IMAGE_W, IMAGE_H, 1)
+
+  print(X.shape)
+  print(Y.shape)
+
+  np.save("X", X)
+  np.save("Y", Y)
+
 
 
 if __name__ == '__main__':
--- train.py
+++ train.py
@@ -1,17 +1,78 @@
 import os
 os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 
+import numpy as np
+
 import keras
 from keras.datasets import mnist
-from keras.models import Model
+from keras.models import Sequential
 from keras.layers import Input, Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from keras.optimizers import Adam
 from keras.callbacks import TensorBoard
 from keras import backend as K
+#K.set_image_data_format('channels_first')
+
+WIDTH = 160
+HEIGHT = 160
+
 
 
 
 def create_model():
   model = Sequential()
 
-  model.add(Conv2D(128, (3, 3), activation='relu', padding='same')
+  model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(WIDTH, HEIGHT, 1)))
+  model.add(MaxPooling2D((2, 2), padding='same'))
+  model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+  model.add(MaxPooling2D((2, 2), padding='same'))
+
+  model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+  model.add(MaxPooling2D((2, 2), padding='same'))
+  model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+  model.add(MaxPooling2D((2, 2), padding='same'))
+  #model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+  #model.add(MaxPooling2D((2, 2), padding='same'))
+
+  ################################################## 
+
+  #model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+  #model.add(UpSampling2D((2, 2)))
+  model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+  model.add(UpSampling2D((2, 2)))
+  model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+  model.add(UpSampling2D((2, 2)))
+
+  model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+  model.add(UpSampling2D((2, 2)))
+  model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+  model.add(UpSampling2D((2, 2)))
+
+  model.add(Conv2D(1, (3, 3), activation='sigmoid', padding='same'))
+
+
+
+
+  opt=Adam(lr=0.001, decay=0.00001)
+  model.compile(loss="mean_squared_error", optimizer=opt)
+
+  model.summary()
+  return model
+
+
+if __name__ == '__main__':
+  model = create_model()
+ 
+  print("Loading Data...")
+  X = np.load("X.npy")
+  Y = np.load("Y.npy")
+
+  X = np.divide(X, 255.0)
+  Y = np.divide(Y, 255.0)
+
+  EPOCHS = 100
+
+  model.fit(X, Y, epochs=EPOCHS, batch_size=256)
+
+  model.save("net-{}E.model".format(EPOCHS))
+
